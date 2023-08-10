afa7b9bc3c085d85758ff26f26e8718266a41a54
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-10 17:58:16 +0800

    Update dependencies

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { type JsonValue } from "https://deno.land/std@0.197.0/json/common.ts";
+import { type JsonValue } from "https://deno.land/std@0.198.0/json/common.ts";
 import { ExFetch, type ExFetchOptions, type ExFetchRetryOptions } from "https://deno.land/x/exfetch@v0.1.2/exfetch.ts";
 type CommonErrorType = string | Error | RangeError | ReferenceError | SyntaxError | TypeError;
 /**
